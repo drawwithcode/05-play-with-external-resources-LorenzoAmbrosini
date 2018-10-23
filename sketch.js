42d14352adaa28880f7ba2ce7f8ee3fc42c91712
Lorenzo Ambrosini <lorenzo.ambrosini@mail.polimi.it>
2018-10-23 11:33:13 +0200

    404 problem
    ritorno a solo eye

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ var song;
 
 function preload() {
   myImage = loadImage('./assets/Armstrong.png')
-  occhio_sx = loadImage('./assets/Armstrong_Eye2.png');
-  occhio_dx = loadImage('./assets/Armstrong_Eye2.png');;
+  occhio_sx = loadImage('./assets/Armstrong_Eye.png');
+  occhio_dx = loadImage('./assets/Armstrong_Eye.png');;
   trumpet = loadImage('./assets/Armstrong_Trumpet.png')
 
   song = loadSound('./assets/Sound.mp3');
